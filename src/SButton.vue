6713e76c1a79e205de86f32145191483c986a19e
jiayinkong <ry1n.kong@gmail.com>
2022-10-12 21:18:44 +0800

    add: 添加template单文件组件

---
+++ src/SButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <button>button template</button>
+</template>
+
+<script lang="ts">
+  export default {
+    
+  }
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
-import SButton from './button';
+// import SButton from './button';
+import SButton from './SButton.vue';
 
 createApp(SButton).mount('#app');
