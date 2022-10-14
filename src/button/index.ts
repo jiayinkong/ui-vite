2bc32a1d5c240ac2d71a985ee4a4d1bc9ea48c74
jiayinkong <ry1n.kong@gmail.com>
2022-10-14 12:42:22 +0800

    add: 添加 原子样式

--- src/button/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineComponent, h } from 'vue';
-
-export default defineComponent({
-  name: 'HButton',
-  render() {
-    return h('button', null, 'MyButton')
-  }
-})
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-
+import Unocss from './config/unocss';
 const rollupOptions = {
   external: ["vue", "vue-router"],
   output: {
@@ -12,11 +12,16 @@ const rollupOptions = {
 }
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(), 
+    vueJsx(),
+    Unocss()
+  ],
   // 添加库模式配置
   build: {
     rollupOptions,
     minify: false,
+    cssCodeSplit: true,
     lib: {
       entry: './src/entry.ts',
       name: 'uiVite',
