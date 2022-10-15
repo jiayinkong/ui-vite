1113f5649774c55248363fdeabccb2cd3b6b0bb4
jiayinkong <ry1n.kong@gmail.com>
2022-10-15 22:57:24 +0800

    feat: 添加代码提交规范

--- src/button/__test__/Button.spec.ts
+++ src/button/__test__/Button.spec.ts
@@ -1,42 +1,52 @@
-import Button from '../Button';
+import Button from "../Button";
 
-import { shallowMount } from '@vue/test-utils';
-import { describe, expect, test } from 'vitest';
+import { shallowMount } from "@vue/test-utils";
+import { describe, expect, test } from "vitest";
 
 // 测试分组
-describe('Button', () => {
-  test('mount @vue/test-utils', () => {
+describe("Button", () => {
+  test("mount @vue/test-utils", () => {
     const wrapper = shallowMount(Button, {
       slots: {
-        default: 'Button'
-      }
-    })
-  
-    expect(wrapper.text()).toBe('Button');
-  })
-})
+        default: "Button",
+      },
+    });
 
-describe('color', () => {
-  test('default', () => {
+    expect(wrapper.text()).toBe("Button");
+  });
+});
+
+describe("color", () => {
+  test("default", () => {
     const wrapper = shallowMount(Button, {
       slots: {
-        default: 'Button'
-      }
-    })
+        default: "Button",
+      },
+    });
 
-    expect(wrapper.classes().map(v => v.replace('\n', '')).includes('bg-blue-500')).toBe(true);
-  })
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-blue-500")
+    ).toBe(true);
+  });
 
-  test('red', () => {
+  test("red", () => {
     const wrapper = shallowMount(Button, {
       slots: {
-        default: 'Button'
+        default: "Button",
       },
       props: {
-        color: 'red'
-      }
-    })
+        color: "red",
+      },
+    });
 
-    expect(wrapper.classes().map(v => v.replace('\n', '')).includes('bg-red-500')).toBe(true);
-  })
-})
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-red-500")
+    ).toBe(true);
+  });
+});
--- src/entry.ts
+++ src/entry.ts
@@ -1,17 +1,13 @@
-import HButton from './button';
-import TButton from './SButton.vue';
-import JButton from './JSXButton';
+import HButton from "./button";
+import TButton from "./SButton.vue";
+import JButton from "./JSXButton";
 
-export {
-  HButton,
-  TButton,
-  JButton
-}
+export { HButton, TButton, JButton };
 
 export default {
   install(app) {
     app.component(HButton.name, HButton);
     app.component(TButton.name, TButton);
     app.component(JButton.name, JButton);
-  }
-}
+  },
+};
